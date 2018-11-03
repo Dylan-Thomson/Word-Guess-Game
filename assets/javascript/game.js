e2d7ed99a6c2465cf2144463bcf203ae70f785fc
unknown <djthomson88@gmail.com>
2018-11-03 09:47:41 -0400

    Some pseudocode for game logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// Press any key to start
+// Display Wins
+// Pick a word from list
+// Display word like _ _ _ _
+// As user guesses reveal letters
+// Number of guesses remaining
+// Display letters already guessed
+// After win or loss pick new word and play again
+
+var game = {
+    words : [],
+    currentWord : [],
+    wins : 0,
+    guessesRemaining : 10,
+    guesses : [],
+    pickWord : function() {},
+    newGame : function() {},
+    testGuess : function() {},
+
+}
