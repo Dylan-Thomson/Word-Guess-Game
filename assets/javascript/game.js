e79c7616f891383f83a545a6b8eecab1db9a1bb7
unknown <djthomson88@gmail.com>
2018-11-06 18:10:49 -0500

    Refactor all DOM manipulation into updateDisplay() function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ var game = {
     words : ["cat", "dog", "potato", "moooo", "orangotang"],
     currentWord : "",
     wordDisplay : "",
+    currentGuess : "",
+    gameState : "",
     wins : 0,
     losses: 0,
     guessesRemaining : 0,
@@ -11,31 +13,28 @@ var game = {
 
     // Pick a random word
     pickWord : function() {
-        this.currentWord = this.words[Math.floor(Math.random() * this.words.length)];
-        document.getElementById("currentWord").textContent = this.currentWord;
-
+        this.currentWord = this.words[Math.floor(Math.random() * this.words.length)];   
         this.wordDisplay = new Array(this.currentWord.length + 1).join( "_" );
-        document.getElementById("wordDisplay").textContent = this.wordDisplay;
     },
-
+    
     // Pick a new word, clear guesses and update guesses remaining
     newGame : function() {
         this.gameRunning = true;
-        curentWord = this.pickWord();
+        this.curentWord = this.pickWord();
+        this.currentGuess = "";
         this.guesses = [];
         this.guessesRemaining = 10;
-        document.getElementById("gameStatus").textContent = "";
-        document.getElementById("guesses").textContent = this.guesses;
-        document.getElementById("guessesRemaining").textContent = this.guessesRemaining;
-        document.getElementById("guess").textContent = "";
+        this.gameState = "";
+        
+        this.updateDisplay();
     },
-
+    
+    // TODO: REFACTOR AND FIX CASE WHERE USER LOSES DESPITE PICKING FINAL LETTER ON LAST GUESS
     testGuess : function(letter) {
         // If we haven't already guessed this letter, update guesses and test
         if(!this.guesses.includes(letter)) {
-            document.getElementById("guess").textContent = letter;
             this.updateGuesses(letter);
-
+            
             // If we haven't already run out of guesses
             if(this.guessesRemaining > 0) {
                 // If current word contains our guess
@@ -47,13 +46,12 @@ var game = {
                             indices.push(i);
                         }
                     }
-
+                    
                     //Update letters of wordDisplay at these indexes and display revealed letters
                     for(var i = 0; i < indices.length; i++) {
                         this.wordDisplay = this.wordDisplay.substr(0, indices[i]) + letter + this.wordDisplay.substr(indices[i] + 1);
                     }
-                    document.getElementById("wordDisplay").textContent = this.wordDisplay;
-
+                    
                     //Compare new wordDisplay with currentWord to see if we won the game
                     if(this.currentWord === this.wordDisplay) {
                         this.win();
@@ -66,41 +64,36 @@ var game = {
             }
         }
     },
-
+    
     // Add current letter to guesses, reduces guesses remaining
     updateGuesses : function(letter) {
+        this.currentGuess = letter;
         this.guesses.push(letter);
         this.guessesRemaining--;
-        this.displayGuesses();
-    },
-    
-    displayGuesses : function() {
-        document.getElementById("guesses").textContent = this.guesses.join(" ").toUpperCase();
-        document.getElementById("guessesRemaining").textContent = this.guessesRemaining;
     },
     
     lose : function() {
-        // alert("You lost! The word was: " + this.currentWord);
-        document.getElementById("gameStatus").textContent = "You lost!";
         this.losses++;
-        document.getElementById("losses").textContent = this.losses;
-        // this.newGame();
+        this.gameState = "You lost. The word you were trying to guess is: " + this.currentWord;
         this.gameRunning = false;
     },
     
     win : function() {
-        // alert("You won! The word was: " + this.currentWord);
-        document.getElementById("gameStatus").textContent = "Winner!";
         this.wins++;
-        document.getElementById("wins").textContent = this.wins;
-        // this.newGame();
+        this.gameState = "You won! The word you were trying to guess is: " + this.currentWord;
         this.gameRunning = false;
     },
     
     updateDisplay() {
+        document.getElementById("currentWord").textContent = this.currentWord;
+        document.getElementById("wordDisplay").textContent = this.wordDisplay;
         document.getElementById("guesses").textContent = this.guesses.join(" ").toUpperCase();
         document.getElementById("guessesRemaining").textContent = this.guessesRemaining;
-
+        document.getElementById("guess").textContent = this.currentGuess;
+        document.getElementById("wordDisplay").textContent = this.wordDisplay;
+        document.getElementById("gameStatus").textContent = this.gameState;
+        document.getElementById("wins").textContent = this.wins;
+        document.getElementById("losses").textContent = this.losses;
     }
 }
 
@@ -112,7 +105,7 @@ window.onload = function() {
             var letter = event.key.toLowerCase();
             if(letter.length === 1 && letter.match(/[a-z]/i)) {
                 game.testGuess(letter);
-                // game.updateDisplay();
+                game.updateDisplay();
             }
         }
     }
